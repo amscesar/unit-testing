efcda4611ec7c44506ba0fe6f94fd2816751592e
alessandro meireles <alessandromeireles@gmail.com>
2020-02-29 13:30:23 -0300

    Add test double

---
+++ src/main/java/school/cesar/unit/Message.java
@@ -0,0 +1,13 @@
+package school.cesar.unit;
+
+public class Message {
+        private String msg;
+        private String rec;
+
+        public String getMsg(){
+            return msg;
+        }
+        public String getRec (){
+            return rec;
+        }
+ }
