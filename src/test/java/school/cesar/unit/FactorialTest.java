2cee5dc19cfd7923d01c2d497a6f7d2a9ab5e84f
alessandro meireles <alessandromeireles@gmail.com>
2020-02-29 15:26:54 -0300

    add mock

--- src/test/java/school/cesar/unit/FactorialTest.java
+++ src/test/java/school/cesar/unit/FactorialTest.java
@@ -4,8 +4,24 @@ import com.sun.javafx.css.CalculatedValue;
 import com.sun.org.apache.xpath.internal.objects.XNumber;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+//@ExtendWith(MockitoExtension.class)
 public class FactorialTest {
+
+ //   @Mock Factorial factorial;
+
+//    @Test
+//    public void calculate_callAmountForEightMock (){
+//        factorial.calculate(8);
+//       verify(factorial, times(8)).calculate(any(Integer.class));
+//    }
+
     @Test
     public void calculate_callAmountForEight() {
         class FactorialSpy extends Factorial {
