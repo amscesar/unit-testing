51462cd14799fd5e2b9297a6f9566bcec9e9201d
alessandro meireles <alessandromeireles@gmail.com>
2020-02-29 14:18:41 -0300

    Add test spies

---
+++ src/main/java/school/cesar/unit/Factorial.java
@@ -0,0 +1,11 @@
+package school.cesar.unit;
+
+public class Factorial {
+
+    public int calculate(int number){
+        if (number <= 1) {
+            return  number;
+        }
+        return number * calculate(number -1);
+    }
+}
---
+++ src/test/java/school/cesar/unit/FactorialTest.java
@@ -0,0 +1,24 @@
+package school.cesar.unit;
+
+import com.sun.javafx.css.CalculatedValue;
+import com.sun.org.apache.xpath.internal.objects.XNumber;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class FactorialTest {
+    @Test
+    public void calculate_callAmountForEight() {
+        class FactorialSpy extends Factorial {
+            public int countCalls = 0;
+
+            @Override
+            public int calculate(int number) {
+                countCalls++;
+                return super.calculate(number);
+            }
+        }
+        FactorialSpy spy = new FactorialSpy();
+        spy.calculate(8);
+        Assertions.assertEquals(8, spy.countCalls);
+    }
+}
